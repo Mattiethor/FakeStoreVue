e014b59a9aa46f63eabf14e18d94c0a9d0354a71
Matthías Enok Þórarinsson <met1@nemandi.ntv.is>
2022-05-22 12:42:16 +0000

    Work stared on home page, Moving my code from replit to vs code

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,162 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+     <router-link to ="/"><a class="navbar-brand" href="#">FakeStore</a></router-link> 
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+           <router-link to ="/"><a class="nav-link" href="#"
+              >Home <span class="sr-only">(current)</span></a
+            ></router-link> 
+          </li>
+          <li class="nav-item">
+           <router-link to="/about"><a class="nav-link" href="#">Kitchen</a></router-link> 
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Bedroom</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Summer</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <!-- Navbar end -->
+    <router-view />
+    <footer class="text-center text-lg-start bg-light text-muted">
+      <!-- Section: Social media -->
+      <section
+        class="
+          d-flex
+          justify-content-center justify-content-lg-between
+          p-4
+          border-bottom
+        "
+      >
+        <!-- Left -->
+        <div class="me-5 d-none d-lg-block">
+          <span>Get connected with us on social networks:</span>
+        </div>
+        <!-- Left -->
+
+        <!-- Right -->
+        <div>
+          <a href="" class="me-4 text-reset">
+            <i class="fab fa-facebook-f"></i>
+          </a>
+          <a href="" class="me-4 text-reset">
+            <i class="fab fa-twitter"></i>
+          </a>
+          <a href="" class="me-4 text-reset">
+            <i class="fab fa-google"></i>
+          </a>
+          <a href="" class="me-4 text-reset">
+            <i class="fab fa-instagram"></i>
+          </a>
+          <a href="" class="me-4 text-reset">
+            <i class="fab fa-linkedin"></i>
+          </a>
+          <a href="" class="me-4 text-reset">
+            <i class="fab fa-github"></i>
+          </a>
+        </div>
+        <!-- Right -->
+      </section>
+      <!-- Section: Social media -->
+
+      <!-- Section: Links  -->
+      <section class="">
+        <div class="container text-center text-md-start mt-5">
+          <!-- Grid row -->
+          <div class="row mt-3">
+            <!-- Grid column -->
+            <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+              <!-- Content -->
+              <h6 class="text-uppercase fw-bold mb-4">
+                <i class="fas fa-gem me-3"></i>FakeStore
+              </h6>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
+                maxime aliquid eaque eligendi harum ab!
+              </p>
+            </div>
+            <!-- Grid column -->
+
+            <!-- Grid column -->
+            <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+              <!-- Links -->
+              <h6 class="text-uppercase fw-bold mb-4">Products</h6>
+              <p>
+                <a href="#!" class="text-reset">Summer</a>
+              </p>
+              <p>
+                <a href="#!" class="text-reset">Kitchen</a>
+              </p>
+              <p>
+                <a href="#!" class="text-reset">Bedroom</a>
+              </p>
+            </div>
+            <!-- Grid column -->
+
+            <!-- Grid column -->
+            <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+              <!-- Links -->
+              <h6 class="text-uppercase fw-bold mb-4">Useful links</h6>
+              <p>
+                <a href="#!" class="text-reset">About</a>
+              </p>
+              <p>
+                <a href="#!" class="text-reset">Applications</a>
+              </p>
+              <p>
+                <a href="#!" class="text-reset">Contact us</a>
+              </p>
+            </div>
+            <!-- Grid column -->
+
+            <!-- Grid column -->
+            <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+              <!-- Links -->
+              <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
+              <p><i class="fas fa-home me-3"></i> Reykjavik, RVK 101, IS</p>
+              <p>
+                <i class="fas fa-envelope me-3"></i>
+                info@example.com
+              </p>
+              <p><i class="fas fa-phone me-3"></i> + 01 234 567 88</p>
+              <p><i class="fas fa-print me-3"></i> + 01 234 567 89</p>
+            </div>
+            <!-- Grid column -->
+          </div>
+          <!-- Grid row -->
+        </div>
+      </section>
+      <!-- Section: Links  -->
+
+      <!-- Copyright -->
+      <div
+        class="text-center p-4"
+        style="background-color: rgba(0, 0, 0, 0.05)"
+      >
+        © 2021 Copyright:
+        <a class="text-reset fw-bold" href="https://mdbootstrap.com/"
+          >MDBootstrap.com</a
+        >
+      </div>
+      <!-- Copyright -->
+    </footer>
+    <!-- Footer -->
   </div>
 </template>
 
@@ -17,16 +169,5 @@
   color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
-}
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,60 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-8">
+          <img
+            src="https://media.istockphoto.com/photos/garden-patio-decorated-with-scandinavian-wicker-sofa-and-coffee-table-picture-id1208657520"
+            class="img-fluid"
+            alt="Responsive image"
+          />
+        </div>
+        <div class="col-md-4">
+          <h2>Our motto</h2>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi,
+            quisquam iste laudantium minima officiis sint? Corrupti nemo
+            perspiciatis quia? Itaque corporis laudantium ratione quam sit
+            voluptatum neque, exercitationem reprehenderit harum.
+          </p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-4">
+          <img
+            src="https://media.istockphoto.com/photos/wooden-chairs-at-table-in-bright-open-space-interior-with-lamp-next-picture-id968086564"
+            class="img-fluid"
+            alt="Responsive image"
+          />
+        </div>
+        <div class="col-md-4">
+          <img
+            src="https://media.istockphoto.com/photos/scandinavian-bedroom-interior-stock-photo-picture-id1264323513"
+            class="img-fluid"
+            alt="Responsive image"
+          />
+        </div>
+        <div class="col-md-4">
+          <img
+            src="https://media.istockphoto.com/photos/selecting-person-and-building-team-business-people-relationship-picture-id1140300365"
+            class="img-fluid"
+            alt="Responsive image"
+          />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+//import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'Home',
+ /* name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  }, */
+};
+
 </script>
